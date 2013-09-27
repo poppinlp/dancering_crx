7fde701fa5a39140bb9770fc540caa57cd0cea70
PoppinL <poppinlp@gmail.com>
2013-09-28 01:00:02 +0800

    init

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-dancering_crx
-=============
-
+##dancering\_crx
 Dancering网站的音乐下载chrome插件
+
+###当前版本:
+
+####0.1.0
+- 原型，提供一键下载的功能
---
+++ js/bg.js
@@ -0,0 +1,24 @@
+var menu = chrome.contextMenus.create({
+    'title' : 'use Dusic to download',
+    'contexts' : ['link'],
+    'onclick' : download,
+    'documentUrlPatterns' : ['http://www.dancering.cn/*'],
+    'targetUrlPatterns' : ['http://www.dancering.cn/music_show-*']
+});
+
+function download(info) {
+    var url = info.linkUrl;
+    var temp = url.indexOf('music_show-');
+    if(temp != -1) {
+        url = url.substr(temp + 11);
+        url = url.substr(0, url.indexOf('.html'));
+        url = 'http://www.dancering.cn/source/module/forum/get_filecont.php?aid=' + base64encode(url);
+        if(chrome.downloads != null) {
+            chrome.downloads.download({'url':url})
+        } else {
+            chrome.tabs.create({'url':url})
+        }
+    } else {
+        alert('目前只能下载单个曲子哦，整个页面列表神马的，再等等吧 >_<');
+    }
+}
