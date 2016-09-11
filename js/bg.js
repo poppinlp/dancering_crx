f0ae25dca8319cca17f1f3e6ce4530e9851687ec
PoppinL <poppinlp@gmail.com>
2016-09-11 20:56:39 +0800

    up config

--- js/bg.js
+++ js/bg.js
@@ -1,4 +1,4 @@
-var menu = chrome.contextMenus.create({
+chrome.contextMenus.create({
     'title' : 'use Dusic to download',
     'contexts' : ['link'],
     'onclick' : download,
@@ -9,11 +9,11 @@ var menu = chrome.contextMenus.create({
 function download(info) {
     var url = info.linkUrl;
     var temp = url.indexOf('music_show-');
-    if(temp != -1) {
+    if(temp !== -1) {
         url = url.substr(temp + 11);
         url = url.substr(0, url.indexOf('.html'));
         url = 'http://www.dancering.cn/source/module/forum/get_filecont.php?aid=' + base64encode(url);
-        if(chrome.downloads != null) {
+        if(chrome.downloads !== null) {
             chrome.downloads.download({'url':url})
         } else {
             chrome.tabs.create({'url':url})
